d3a4ee89ee4a9cb4b38b52cc84a1a68bc6eca9b4
Firzad Ahammed <firzad.ahmmed@gmail.com>
2020-03-11 11:54:37 +1100

    updated packages

---
+++ client/src/App_Sample_copy.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+
+import axios from './axios';
+
+import Form from './components/toBeDeleted_Sample/Form';
+import Item from './components/toBeDeleted_Sample/Item';
+
+import { ServerData, ServerResponse } from './interfaces/toBeDeleted_Sample_interface';
+
+class App extends React.Component {
+    state = {
+        data: [],
+        showPopup: false
+    };
+
+    togglePopup = () => {
+        this.setState({
+            showPopup: !this.state.showPopup
+        });
+    }
+
+    componentDidMount() {
+        this.loadData()
+    }
+
+    loadData = () => {
+        axios.get(`/apps`).then(
+            (res: ServerResponse) => {
+                const data = res.data;
+                console.log(res);
+                this.setState({ data });
+            });
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <article className="row">
+                    {this.state.data.map((app: ServerData) => <Item key={app.id} {...app} updateParent={this.loadData} />)}
+                    <button
+                        type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        Add Application
+                    </button>
+                    <Form closePopup={this.togglePopup} updateParent={this.loadData} />
+                </article>
+            </div>
+        );
+    }
+}
+
+export default App;
