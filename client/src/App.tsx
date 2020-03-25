6b4d3f284ecf29485352b127d9897ce50c664caf
JamesCG <30425961+WolfgangAvitus@users.noreply.github.com>
2020-03-26 10:08:01 +1100

    Root redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { LandingPage } from "./components/LandingPage";
 import { Admin } from "./components/Admin";
 import { Customer } from "./components/Customer";
@@ -13,6 +13,7 @@ class App extends React.Component {
         return (
             <Router>
                 <div>
+                    <Route exact path="/"> <Redirect to="/home"/> </Route>
                     <Route exact path="/home" component={LandingPage} />
                     <Route path="/admin" component={Admin} />
                     <Route path="/customer" component={Customer} />
