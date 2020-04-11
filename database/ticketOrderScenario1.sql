79d18394f970dcac4b72646ab1e3fd6cc984465e
JamesCG <30425961+WolfgangAvitus@users.noreply.github.com>
2020-04-11 10:33:18 +1000

    Update ticketOrderScenario1.sql

--- database/ticketOrderScenario1.sql
+++ database/ticketOrderScenario1.sql
@@ -29,8 +29,8 @@ update table_details set table_status='Ordered' where table_number=1;
 insert into ticket (session_id, ticket_timestamp, table_number)
 	values (1, '2020-04-08 11:00:00', 1); --create the ticket
 insert into ticket_item(ticket_id, menu_id, remark, item_status)
-	values (1, 1001, '', 'Delivered'); --create the food item
+	values (2, 1001, '', 'Delivered'); --create the food item
 insert into ticket_item(ticket_id, menu_id, remark, item_status)
-	values (1, 1003, 'extra pepper', 'Delivered');
+	values (2, 1003, 'extra pepper', 'Delivered');
 insert into ticket_item(ticket_id, menu_id, remark, item_status)
-	values (1, 1004, 'no salt thanks', 'Delivered');
+	values (2, 1004, 'no salt thanks', 'Delivered');
