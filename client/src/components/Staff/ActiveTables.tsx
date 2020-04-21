b6d033464e3ef9cca0fcdbc09ea57e9263480f3e
JamesCG <30425961+WolfgangAvitus@users.noreply.github.com>
2020-04-21 23:14:55 +1000

    staff calls on database refactor, less db strain

--- client/src/components/Staff/ActiveTables.tsx
+++ client/src/components/Staff/ActiveTables.tsx
@@ -1,75 +1,18 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
-import LocalDiningIcon from '@material-ui/icons/LocalDining';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import DoneIcon from '@material-ui/icons/Done';
-import AnnouncementIcon from '@material-ui/icons/Announcement';
-import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 import TableDialogue from './TableDialogue'
-import axios from '../../axios';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
-
-//import interfaces
-import {Tables, ServerResponse} from "../../interfaces/table"
-
+import axios from '../../axios';
 
 export default function ActiveTables(props){
-	const setAssistanceTables = props.setAssistanceTables
+	const active_tables = props.active_tables
+	const table_icons = props.table_icons
 
-	function createIcons(data){
-		const icons = data.map((table) => {
-				if (table.assistance === true){
-					return <Avatar style={{backgroundColor: "FIREBRICK"}}><AnnouncementIcon/></Avatar>
-				}
-				switch(table.table_status){
-					case 'Seated':
-						return <Avatar style={{backgroundColor: "Thistle"}}><MenuBookIcon/></Avatar>
-					case 'Paying':
-						return <Avatar style={{backgroundColor: "PALETURQUOISE"}}><AttachMoneyIcon/></Avatar>
-					case 'Ready To Deliver':
-						return <Avatar style={{backgroundColor: "FIREBRICK"}}><LocalDiningIcon/></Avatar>
-					case 'Delivered':
-						return <Avatar style={{backgroundColor: "DARKCYAN"}}><DoneIcon/></Avatar>
-					case 'Ordered':
-						return <Avatar style={{backgroundColor: "darkcyan"}}><MenuBookIcon /></Avatar>
-					default:
-						return <Avatar style={{backgroundColor: "Thistle"}}><MenuBookIcon /></Avatar>
-				}
-		})
-		return icons
-	}
-
-
-	const [active_tables, setActiveTables] = React.useState<Tables | any>([]);
-	const [table_icons, setTableIcons] = React.useState([<MenuBookIcon />])
-
-	useEffect(() => {
-		const interval = setInterval(()=>{
-			axios.get(`Tables/active`).then(
-	            (res: ServerResponse) => {
-	                const data = res.data;
-	                setTableIcons(createIcons(data))
-
-	                setActiveTables(data);
-
-	                let new_assistance : Tables[] = []
-	                for (let i = 0; i < active_tables.length; i++){
-	                	if (active_tables[i].assistance === true){
-	                		new_assistance.push(active_tables[i])
-	                	}
-	                }
-	                setAssistanceTables(new_assistance)
-	            }
-	        )
-		}, 1000)
-		return () => clearInterval(interval)
-	})
 	
 	const handleDeliverFood = table_number => e =>{
 		axios.patch('Tables/Delivered/' + table_number.toString())
